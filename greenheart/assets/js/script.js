0e3f8159ebe234f3ed4fa02b416308b337b94712
denisa-966 <denisarabadiya@gmail.com>
2026-07-13 17:07:05 +0530

    commit

--- greenheart/assets/js/script.js
+++ greenheart/assets/js/script.js
@@ -1,26 +1,26 @@
-let header = document.querySelector("header");
-let menuBtn = document.querySelector(".menu-btn");
-let closeBtn = document.querySelector(".close-btn");
-let side = document.querySelector(".right-side");
-let headerLogo = document.querySelector(".img-logo");
-let headerNavIcon = document.querySelector(".header-nav-icon");
-let subMenu = document.querySelector(".sub-menu");
+// let header = document.querySelector("header");
+// let menuBtn = document.querySelector(".menu-btn");
+// let closeBtn = document.querySelector(".close-btn");
+// let side = document.querySelector(".right-side");
+// let headerLogo = document.querySelector(".img-logo");
+// let headerNavIcon = document.querySelector(".header-nav-icon");
+// let subMenu = document.querySelector(".sub-menu");
 
-menuBtn.addEventListener("click", () => {
-  header.classList.add("active");
-  side.classList.add("active");
-  headerLogo.classList.add("active");
-  closeBtn.style.display = "block";
-  menuBtn.style.display = "none";
-});
+// menuBtn.addEventListener("click", () => {
+//   header.classList.add("active");
+//   side.classList.add("active");
+//   headerLogo.classList.add("active");
+//   closeBtn.style.display = "block";
+//   menuBtn.style.display = "none";
+// });
 
-closeBtn.addEventListener("click", () => {
-  header.classList.remove("active");
-  side.classList.remove("active");
-  headerLogo.classList.remove("active");
-  closeBtn.style.display = "none";
-  menuBtn.style.display = "block";
-});
+// closeBtn.addEventListener("click", () => {
+//   header.classList.remove("active");
+//   side.classList.remove("active");
+//   headerLogo.classList.remove("active");
+//   closeBtn.style.display = "none";
+//   menuBtn.style.display = "block";
+// });
 
 document.querySelectorAll(".sub-nav").forEach((item) => {
   const headerNavIcon = item.querySelector(".header-nav-icon");
@@ -61,6 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", function () {
   var splide = new Splide(".benefits-silder.splide", {
     perpage: 1,
+    arrows: false,
     pagination: false,
   });
   splide.mount();
