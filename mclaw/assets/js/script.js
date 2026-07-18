f75a7bd5cedd7a1f497965c09ec74649b72d0e89
denisa-966 <denisarabadiya@gmail.com>
2026-07-18 18:22:37 +0530

    create faq section

--- mclaw/assets/js/script.js
+++ mclaw/assets/js/script.js
@@ -1,6 +1,7 @@
 let menuBtn = document.querySelector(".menu-btn");
 let closeBtn = document.querySelector(".close-btn");
 let menuLink = document.querySelector(".header-wrapper-navbar-link");
+let circleArrow = document.querySelector(".two-column-wrapper-left-side-content-circle");
 
 menuBtn.addEventListener("click", () => {
   closeBtn.style.display = "block";
@@ -11,6 +12,17 @@ closeBtn.addEventListener("click", () => {
   closeBtn.style.display = "none";
   menuBtn.style.display = "block";
   menuLink.style.display = "none";
+});
+
+const faqs = document.querySelectorAll(".faq-header");
+
+faqs.forEach((faq) => {
+  faq.querySelector(".faq-content").addEventListener("click", () => {
+    faqs.forEach((item) => {
+      if (item !== faq) item.classList.remove("active");
+    });
+    faq.classList.toggle("active");
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
